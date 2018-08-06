cd60e8a420d28d7772852473adc585143de7308a
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-06 23:08:14 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   post '/recipes' do
    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
    @recipe.save
-   redirect '/recipes'
+   redirect "/recipes/#{@recipe.id}"
   end
 
   get '/recipes' do
